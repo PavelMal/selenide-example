053e35a232aa353c2e1d1fb4dfedee6a58681f94
pavelmal <mashino@mail.ru>
2023-09-21 18:09:15 +0300

    add functionality to check logo in searched results page

--- src/main/java/com/qa/page/SearchedResultPage.java
+++ src/main/java/com/qa/page/SearchedResultPage.java
@@ -1,5 +1,16 @@
 package com.qa.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class SearchedResultPage {
-//    HeaderDesktop-Main
+    private final SelenideElement headerDesktopLogo = $("[class=\"HeaderDesktopLogo HeaderDesktopLogo_lang_ru HeaderDesktop-Logo\"]");
+
+    @Step("Header desktop logo should be: {condition}")
+    public void headerDesktopLogoShouldBe(Condition condition) {
+        headerDesktopLogo.shouldBe(condition);
+    }
 }
