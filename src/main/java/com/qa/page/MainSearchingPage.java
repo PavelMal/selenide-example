8ec87a58e89b808d4b504aa91659c7972eb2f688
pavelmal <mashino@mail.ru>
2023-09-21 17:46:21 +0300

    add functionality for clicking on login button

--- src/main/java/com/qa/page/MainSearchingPage.java
+++ src/main/java/com/qa/page/MainSearchingPage.java
@@ -1,15 +1,24 @@
 package com.qa.page;
 
+import com.codeborne.selenide.SelenideElement;
 import com.qa.component.Searching;
 import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class MainSearchingPage {
 
     private final Searching searching = new Searching();
+    private final SelenideElement loginButton = $("[data-statlog=\"headline.enter\"]");
 
     @Step("Input text into searching: {text}")
     public void searchByText(String text) {
         searching.inputTextToSearch(text);
         searching.clickOnFindButton();
+    }
+
+    @Step("Click on login button")
+    public void clickOnLogIn() {
+        loginButton.click();
     }
 }
