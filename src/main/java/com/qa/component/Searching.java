361c60ae242c6e2eb029d7388b0a93df60708840
pavelmal <mashino@mail.ru>
2023-09-22 18:38:07 +0300

    add functionality for clearing data in searching

--- src/main/java/com/qa/component/Searching.java
+++ src/main/java/com/qa/component/Searching.java
@@ -11,6 +11,7 @@ public class Searching {
     private final SelenideElement searching = $("[class=\"search3__inner\"]");
     private final SelenideElement textInput = $("[id=\"text\"]");
     private final SelenideElement findButton = $("[class=\"search3__button mini-suggest__button\"]");
+    private final SelenideElement clearData = $("[class=\"search3__action search3__clear mini-suggest__input-clear\"]");
 
     @Step("Input text to search: {text}")
     public void inputTextToSearch(String text) {
@@ -25,5 +26,10 @@ public class Searching {
     @Step("Searching element should be: {condition}")
     public void searchingElementShouldBe(Condition condition) {
         searching.shouldBe(condition);
+    }
+
+    @Step("Clear data in searching field")
+    public void clearDataInSearching() {
+        clearData.click();
     }
 }
