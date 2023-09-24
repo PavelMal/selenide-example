6b92a8424c015fbb62f0b853f5a5c655870d0aaf
pavelmal <mashino@mail.ru>
2023-09-24 13:25:00 +0300

    add listener to base test for screenshots attachment

--- src/test/java/com/qa/BaseTest.java
+++ src/test/java/com/qa/BaseTest.java
@@ -1,11 +1,14 @@
 package com.qa;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.qa.page.MainSearchingPage;
 import com.qa.page.PassportPage;
 import com.qa.page.SearchedResultPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.BeforeTest;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class BaseTest {
     protected final MainSearchingPage mainPage = new MainSearchingPage();
@@ -15,5 +18,6 @@ public class BaseTest {
     @BeforeTest
     public void setUpBaseURL() {
         open("https://yandex.com/");
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
     }
 }
